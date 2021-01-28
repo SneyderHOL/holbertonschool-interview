eaa61eab546a43bc8b0c553c08047f39e8af508a
SneyderHOL <1507@holbertonschool.com>
2021-01-28 09:20:38 -0500

    [UPDATE]: evaluate case of box is an empty list

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,7 +4,7 @@
 
 def canUnlockAll(boxes):
     '''method to determine if all boxes can be opened'''
-    if boxes is None:
+    if boxes is None or len(boxes) == 0:
         return False
     boxes_open = [False]*len(boxes)
     boxes_open[0] = True
