155fbfc0a56b763adb02d618cf74282ff2d660b2
SneyderHOL <1507@holbertonschool.com>
2021-01-29 09:13:11 -0500

    [UPDATE]: use iteration instead of recursion

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,17 +4,23 @@
 
 def canUnlockAll(boxes):
     '''method to determine if all boxes can be opened'''
-    if boxes is None or len(boxes) == 0:
-        return True
+    if boxes is None:
+        return False
     boxes_open = [False]*len(boxes)
     boxes_open[0] = True
-    open_box(boxes[0], boxes, boxes_open)
+    for box_number in range(len(boxes)):
+        if boxes_open[box_number]:
+            for key in boxes[box_number]:
+                if key >= len(boxes):
+                    continue
+                boxes_open[key] = True;
+                open_box(boxes[key],boxes_open)
     if False in boxes_open:
         return False
     return True
 
 
-def open_box(box, boxes, boxes_open):
+def open_box(box, boxes_open):
     '''method to open boxes'''
     if len(box) == 0:
         return
@@ -23,4 +29,3 @@ def open_box(box, boxes, boxes_open):
             continue
         if boxes_open[key] is False:
             boxes_open[key] = True
-            open_box(boxes[key], boxes, boxes_open)
