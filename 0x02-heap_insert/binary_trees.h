f195998e0546707944a2b53ee79411dbcc8864e8
SneyderHOL <1507@holbertonschool.com>
2021-02-12 02:04:53 +0000

    Add task 2

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -19,6 +19,6 @@ typedef struct binary_tree_s
 typedef struct binary_tree_s heap_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
-
+heap_t *heap_insert(heap_t **root, int value);
 
 #endif /* BINARY_TREE_H */
