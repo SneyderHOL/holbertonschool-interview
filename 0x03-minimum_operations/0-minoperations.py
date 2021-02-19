28a0f65d77650d4bf1f4a360e195cc8351939c82
SneyderHOL <1507@holbertonschool.com>
2021-02-18 21:54:34 -0500

    [UPDATE]: pep8 style

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -3,6 +3,7 @@
 minOperations module
 """
 
+
 def copy_all(string):
     """Method to copy a string"""
     pass
@@ -15,12 +16,12 @@ def paste(string):
 
 def minOperations(n):
     """Given a number n, minOperationsis a method that calculates the fewest
-       number of operations needed to result in exactly n H characters in 
+       number of operations needed to result in exactly n H characters in
        the file."""
     char = 'H'
     num_copy = 0
     num_paste = 0
-    paste = false
+    paste = False
     operations = n
     limit = 2
     size = 1
