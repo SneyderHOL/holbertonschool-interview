656d6fd8e67b6a63b25d1c2d236b414adaf2ba3c
SneyderHOL <1507@holbertonschool.com>
2021-02-18 21:43:10 -0500

    Add files

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+minOperations module
+"""
+
+def copy_all(string):
+    """Method to copy a string"""
+    pass
+
+def paste(string):
+    """Method to concatenate the same strings"""
+    pass
+
+def minOperations(n):
+    """Given a number n, minOperationsis a method that calculates the fewest
+       number of operations needed to result in exactly n H characters in 
+       the file."""
+    char = 'H'
+    num_copy = 0
+    num_paste = 0
+    paste = false
+    operations = n
+    limit = 2
+    size = 1
+    return n
