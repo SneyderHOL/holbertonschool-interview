1b76e4b331a3a5a5f9fb54658f637a59971fd2a9
SneyderHOL <1507@holbertonschool.com>
2021-02-21 16:04:42 -0500

    [UPDATE]: add task 0

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -4,14 +4,46 @@ minOperations module
 """
 
 
-def copy_all(string):
+def copy_all(string, ret=False):
     """Method to copy a string"""
-    pass
+    if ret:
+        aux = copy_all.counter
+        copy_all.counter = 0
+        return aux
+    copy_all.counter += 1
+    # print(copy_all.counter)
+    return string + ''
 
+copy_all.counter = 0
 
-def paste(string):
+def paste(string, cp_string, ret=False):
     """Method to concatenate the same strings"""
-    pass
+    if ret:
+        aux = paste.counter
+        paste.counter = 0
+        return aux
+    paste.counter += 1
+    # print(paste.counter)
+    return string + cp_string
+
+paste.counter = 0
+
+def reset_counters(c=0, p=0):
+    """Method to reset the counters method"""
+    paste.counter = p
+    copy_all.counter = c
+
+def execution(char, n, limit, min_op):
+    """Method that iterate to find the minimum opeations quantity"""
+    copy = copy_all(char)
+    for i in range(1, limit):
+        char = paste(char, copy)
+    copy = copy_all(char)
+    while len(char) < n:
+        char = paste(char, copy)
+    if len(char) == n:
+        return copy_all('', True) + paste('', '', True)
+    return min_op
 
 
 def minOperations(n):
@@ -19,10 +51,17 @@ def minOperations(n):
        number of operations needed to result in exactly n H characters in
        the file."""
     char = 'H'
-    num_copy = 0
-    num_paste = 0
-    paste = False
-    operations = n
+    min_op = n
+    top_limit = n // 2
+    bottom_limit = len(char)
+    aux = bottom_limit
     limit = 2
-    size = 1
-    return n
+    while (limit <= top_limit):
+        operations = execution(char, n, limit, min_op)
+        reset_counters()
+        if operations < min_op:
+            min_op = operations
+        limit += 1
+    reset_counters()
+    return min_op
+
