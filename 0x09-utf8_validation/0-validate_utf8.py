b744ed97ee0c617591cec76310108abfb7d701a3
SneyderHOL <1507@holbertonschool.com>
2021-04-03 18:49:26 -0500

    [UPDATE]: file is executable

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -5,48 +5,31 @@ validate utf8 module
 """
 
 
-def is_valid_integer(num):
-    """checks if the integer is 1 byte long"""
-    if num > 255 or num < 0:
-        return False
-    return True
-
-
-def valid_init_character_byte(num):
-    """checks if the number is a valid initial character byte"""
-    regex = re.compile('^(1)+')
-    str_byte = format(num, '#010b')[-8:]
-    match = regex.match(str_byte)
-    switcher = {
-        '11': 2,
-        '111': 3,
-        '1111': 4
-    }
-    if match:
-        return switcher.get(match, 1)
-    return False
-
-
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encoding"""
+    regex = '^(1)+'
     num_bytes = 0
     bytes_left = 0
     init = True
     for byte in data:
         if (not init) and bytes_left == 0:
             init = True
-        if not is_valid_integer(byte):
-            return False
-        num_bytes = valid_init_character_byte(byte)
-        if num_bytes is False or (init and num_bytes == 0):
+        num_bytes = 0
+        str_byte = format(byte, '#010b')[-8:]
+        match = re.match(regex, str_byte)
+        if match:
+            num_bytes = len(match.group())
+        if num_bytes > 4:
             return False
         if init:
             bytes_left = num_bytes
-        else:
-            if num_bytes != 0:
-                return False
+        if init and bytes_left == 0:
+            continue
+        if (init and bytes_left == 1) or (not init) and num_bytes != 1:
+            return False
         init = False
         bytes_left -= 1
-    if bytes_left != 0:
-        return False
+    else:
+        if bytes_left != 0:
+            return False
     return True
