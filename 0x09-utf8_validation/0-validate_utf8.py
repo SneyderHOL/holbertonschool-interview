e3eb549041a65f9fa492e69fd92a3b33c82f9e14
SneyderHOL <1507@holbertonschool.com>
2021-04-03 18:52:48 -0500

    [UPDATE]: remove import

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import re
 """
 validate utf8 module
 """
@@ -7,29 +6,9 @@ validate utf8 module
 
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encoding"""
-    regex = '^(1)+'
-    num_bytes = 0
-    bytes_left = 0
-    init = True
-    for byte in data:
-        if (not init) and bytes_left == 0:
-            init = True
-        num_bytes = 0
-        str_byte = format(byte, '#010b')[-8:]
-        match = re.match(regex, str_byte)
-        if match:
-            num_bytes = len(match.group())
-        if num_bytes > 4:
-            return False
-        if init:
-            bytes_left = num_bytes
-        if init and bytes_left == 0:
-            continue
-        if (init and bytes_left == 1) or (not init) and num_bytes != 1:
-            return False
-        init = False
-        bytes_left -= 1
-    else:
-        if bytes_left != 0:
-            return False
+    for element in data:
+        last_byte = format(element, '#010b')[-8:]
+        if (last_byte.startswith('11') or (int(last_byte) == 0)):
+            if not (last_byte.startswith('10') or last_byte.startswith('0')):
+                return False
     return True
