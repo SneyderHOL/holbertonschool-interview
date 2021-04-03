fccc87f45b021ca1eccee30bafbf447cbd61159f
SneyderHOL <1507@holbertonschool.com>
2021-04-03 12:31:22 -0500

    [UPDATE]: short version

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -11,25 +11,8 @@ def validUTF8(data):
     num_bytes = 0
     bytes_left = 0
     init = True
-    for byte in data:
-        if (not init) and bytes_left == 0:
-            init = True
-        num_bytes = 0
-        str_byte = format(byte, '#010b')[-8:]
-        match = re.match(regex, str_byte)
-        if match:
-            num_bytes = len(match.group())
-        if num_bytes > 4:
-            return False
-        if init:
-            bytes_left = num_bytes
-        if init and bytes_left == 0:
-            continue
-        if (init and bytes_left == 1) or (not init) and num_bytes != 1:
-            return False
-        init = False
-        bytes_left -= 1
-    else:
-        if bytes_left != 0:
+    for element in data:
+        last_byte = format(element, '#010b')[-8:]
+        if not (last_byte.startswith('10') or last_byte.startswith('0')):
             return False
     return True
