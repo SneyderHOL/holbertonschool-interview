d44cab520d9f7163dd7309187a6708f9940527c5
SneyderHOL <1507@holbertonschool.com>
2021-04-02 22:01:49 -0500

    [UPDATE]: add content

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -6,4 +6,8 @@ validate utf8 module
 
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encoding"""
+    for element in data:
+        last_byte = format(element, '#010b')[-8:]
+        if not (last_byte.startswith('10') or last_byte.startswith('0')):
+            return False
     return True
