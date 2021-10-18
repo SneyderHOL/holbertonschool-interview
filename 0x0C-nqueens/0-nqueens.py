d27cce0c13e0100a14c819426643e4feec661f49
SneyderHOL <1507@holbertonschool.com>
2021-10-18 15:08:35 -0500

    Add README and file

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for N queens puzzle"""
+
+
+#1- validate number of arguments
+"Usage: nqueens N"
+exit 1
+#2- N must be an integer >= 4
+#not integer
+"N must be a number"
+exit 1
+#integer less than 4
+"N must be at least 4"
+exit 1
+#print every possible solution to the problem
+#One solution per line
+#Format: see example
+#only allowed to import the sys module
---
+++ 0x0C-nqueens/README.md
@@ -0,0 +1,5 @@
+# Project 0x0C N Queens
+
+Project using Python
+
+Interview Preparation
