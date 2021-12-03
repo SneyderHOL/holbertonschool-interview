5c125fe37b5a98c91af0b63f13af9bd44118243d
SneyderHOL <sneyder.amador@koombea.com>
2021-12-02 22:04:36 -0500

    Add header

---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,42 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+/* structures */
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+int n;
+size_t index;
+struct listint_s *next;
+} listint_t;
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+int advanced_binary(int *array, size_t size, int value);
+#endif /* HOLBERTON_H */
